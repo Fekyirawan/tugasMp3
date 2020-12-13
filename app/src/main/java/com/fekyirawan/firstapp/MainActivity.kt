3db0063e39ee9466dea3404bc4461b0985b9ac00
Fekyirawan <71133960+Fekyirawan@users.noreply.github.com>
2020-12-14 03:23:10 +0700

    Initial commit

---
+++ app/src/main/java/com/fekyirawan/firstapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.fekyirawan.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.belajar_constraint_layout)
+
+
+
+    }
+}
